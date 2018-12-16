b3e2edec1e6cc85c74becb25fd2cf655dc164f61
Vishal <vishalsvetrivel@gmail.com>
2018-12-16 14:38:58 +0530

    Lexer, Token and AST abstract start written

---
+++ Project Jaspr/Utils/AbstractSyntaxTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Jaspr.Utils
+{
+    public class AbstractSyntaxTree
+    {
+        public Node root;
+    }
+
+    public abstract class Node {
+        List<Node> branches = new List<Node>();
+
+
+    }
+}
---
+++ Project Jaspr/Utils/Lexer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Jaspr.Utils
+{
+    
+
+    public static class Lexer
+    {
+        public static List<Token> Lex(string code)
+        {
+            List<Token> tokens = new List<Token>();
+
+
+
+            return tokens;
+        }
+
+    }
+
+    public class Token
+    {
+        public string value;
+    }
+}
